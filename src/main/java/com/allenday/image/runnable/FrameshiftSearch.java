f203a460a932bd06294ed7b54a7b958694a6443c
Allen Day <allenday@google.com>
2017-04-20 17:00:42 -0700

    depfix

--- src/main/java/com/allenday/image/runnable/FrameshiftSearch.java
+++ src/main/java/com/allenday/image/runnable/FrameshiftSearch.java
@@ -1,11 +1,56 @@
 package com.allenday.image.runnable;
 
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import java.io.File;
+import java.io.IOException;
+import java.util.ListIterator;
+import java.util.Map.Entry;
+
+import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
-public class FrameshiftSearch {
+import com.allenday.image.ImageFeatures;
+import com.allenday.image.ImageProcessor;
 
+public class FrameshiftSearch {
+    public static void main( String[] args ) throws SolrServerException, IOException {
+      	SolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/frameshift").build();
+
+      	ImageProcessor processor = new ImageProcessor(16,4,false);
+//      	processor.addFile(new File("/Users/allenday/Downloads/gdp_pmv/ph568acd953cf18/"));
+    		processor.addFile(new File("/Users/allenday/Downloads/gdp_pmv/ph568acd953cf18/sec00001.jpg"));
+    		processor.processImages();
+        for (Entry<File,ImageFeatures> e : processor.getResults().entrySet()) {
+        	File image = e.getKey();
+        	ImageFeatures f = e.getValue();
+        	
+					SolrQuery query = new SolrQuery();
+					String qq =							
+							f.getRcompact() + " " +
+							f.getGcompact() + " " +
+							f.getBcompact() + " " +
+							f.getTcompact() + " " +
+							f.getCcompact() + " " +
+							"";
+
+					System.err.println(qq);
+					query.setQuery(qq);
+	        query.set("fl", "id,file_id,score");
+
+					QueryResponse response = solr.query(query);
+					
+					SolrDocumentList list = response.getResults();
+					ListIterator<SolrDocument> resultsIterator = list.listIterator();
+					while (resultsIterator.hasNext()) {
+						SolrDocument result = resultsIterator.next();
+						for (String fieldName : result.getFieldNames()) {
+							System.err.println(fieldName + "\t" + result.getFieldValue(fieldName));
+						}
+					}        	
+        }
+    }
 }
